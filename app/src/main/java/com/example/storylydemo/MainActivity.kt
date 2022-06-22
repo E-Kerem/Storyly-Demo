4c015a177e805863c054896b23fe0db3346598eb
Efe Kerem Kesgin <appsamurai@Efe-Kerem-Kesgins-Macbook-Pro.local>
2022-06-22 11:42:21 +0300

    initial setup

--- app/src/main/java/com/example/storylydemo/MainActivity.kt
+++ app/src/main/java/com/example/storylydemo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.storylydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.appsamurai.storyly.StorylyInit
+import com.appsamurai.storyly.StorylyView
+import com.example.storylydemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.storylyView.storylyInit = StorylyInit(STORYLY_INSTANCE_TOKEN)
+
     }
 }
